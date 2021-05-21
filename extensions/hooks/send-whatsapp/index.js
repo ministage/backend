61896a4a600bc3e897116fe297d142cd159bce16
broodroosterdev <myth.usa538@gmail.com>
2021-05-21 13:54:28 +0200

    Added first files

---
+++ extensions/hooks/send-whatsapp/index.js
@@ -0,0 +1,35 @@
+module.exports = function registerHook({ exceptions }) {
+
+	return {
+		'users.update': async function (input) {
+            if(!Object.keys(input.payload).includes('is_present')){
+                return input;
+            }
+                // Get all users with id, is_present
+                let users = await input.database("directus_users")
+                    .select("id", "is_present", "phone");
+                
+                console.log(users);
+                console.log("is_present aangepast");
+
+
+                let aanwezig = users.filter(user => user.is_present === 1);
+                let afwezig = users.filter(user => user.is_present === 0);
+                
+                console.log("aanwezig " + aanwezig.length);
+                console.log("afwezig " + afwezig.length);
+
+                console.log(input);
+
+                let is_leaving = input.payload.is_present === false;
+                if(aanwezig.length === 1 && is_leaving){
+                    let last_one = aanwezig[0];
+                    console.log(last_one.id);
+                    console.log(last_one.phone);
+                    
+                }
+                
+			return input;
+		},
+	};
+};
